69b1d26e7501d737bce0ee0df0ded40659ceafd8
Gustavo <gustavo.thomaz.ismart@gmail.com>
2021-07-11 00:57:05 -0300

    Implementacao do post e get de postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,19 +14,30 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('https://blogpessoalg.herokuapp.com/usuario/logar',userLogin)
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
-  cadastrar(usuario:Usuario): Observable<Usuario>{
-    return this.http.post<Usuario>('https://blogpessoalg.herokuapp.com/usuario/cadastrar',usuario)
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://blogpessoalg.herokuapp.com/usuario/logar', userLogin)
   }
 
-  logado(){
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>('https://blogpessoalg.herokuapp.com/usuario/cadastrar', usuario)
+  }
+
+  getById(id: number): Observable<Usuario> {
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+    return this.http.get<Usuario>(`https://blogpessoalg.herokuapp.com/usuario/${id}`, this.token)
+  }
+
+  logado() {
     let ok: boolean = false
 
-    if(environment.token != ''){
-      ok =  true
+    if (environment.token != '') {
+      ok = true
     }
 
     return ok
